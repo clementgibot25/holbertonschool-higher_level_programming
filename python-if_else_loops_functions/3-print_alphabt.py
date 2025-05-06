c34396df4873996a8315878f824e44856ac38a88
clementgibot25 <10500@holbertonstudents.com>
2025-05-06 14:14:08 +0200

    used char instead of numbers

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if i != 101 and i != 113:
-        print("%c" % i, end="")
+
+for x in range(ord("a"), ord("z") + 1):
+    if x != ord("e") and x != ord("q"):
+        print("{}".format(chr(x)), end="")
