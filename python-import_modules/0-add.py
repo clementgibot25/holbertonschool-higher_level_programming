207255674b498581476cd4e72b38fb71c83ceb76
clementgibot25 <10500@holbertonstudents.com>
2025-05-07 16:18:15 +0200

    checking if the error comes from me or school checker

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
+from add_0 import add
 
 if __name__ == "__main__":
-    from add_0 import add
-
     a = 1
     b = 2
-    add = add(a, b)
-
-    print("{} + {} = {}".format(a, b, add))
+    print("{} + {} = {}".format(a, b, add(a, b)))
