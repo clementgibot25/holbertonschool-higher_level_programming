04f633a33fefe824405888d9df8b4eb7819a032b
clementgibot25 <10500@holbertonstudents.com>
2025-05-14 15:04:30 +0200

    function that divide elements by elements two lists

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -5,7 +5,8 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             a = my_list_1[i]
             b = my_list_2[i]
-            if not (isinstance(a, int) or isinstance(a, float)) or not (isinstance(b, int) or isinstance(b, float)):
+            if (not (isinstance(a, int) or isinstance(a, float)) or
+                    not (isinstance(b, int) or isinstance(b, float))):
                 print("wrong type")
                 result = 0
             else:
