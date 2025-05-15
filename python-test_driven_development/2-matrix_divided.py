db0c377a092b64347e7fc910d7d6b0f6a3c01c54
clementgibot25 <10500@holbertonstudents.com>
2025-05-15 14:38:24 +0200

    fix pycodestyle

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -23,7 +23,8 @@ def matrix_divided(matrix, div):
             isinstance(x, (int, float)) for x in row
         ) for row in matrix
     ):
-        raise TypeError("matrix must be a matrix of integers/floats")
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
     if len(matrix) > 0:
         size = len(matrix[0])
         if not all(len(row) == size for row in matrix):
