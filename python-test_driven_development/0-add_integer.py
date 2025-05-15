bf354290446cc8a6000ddca90adf23a8ee351898
clementgibot25 <10500@holbertonstudents.com>
2025-05-15 13:53:01 +0200

    fixed pycodestyle

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -2,17 +2,16 @@
 """
 add_integer module
 """
+
+
 def add_integer(a, b=98):
     """
     Adds two integers and returns the result.
-    
     Args:
         a (int): The first integer.
         b (int): The second integer.
-    
     Returns:
         int: The sum of the two integers.
-    
     Raises:
         TypeError: If either `a` or `b` is not an integer.
     """
@@ -20,11 +19,8 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    
-    # Gestion explicite des valeurs infinies
     if isinstance(a, float) and (a == float('inf') or a == float('-inf')):
         raise OverflowError("float overflow")
     if isinstance(b, float) and (b == float('inf') or b == float('-inf')):
         raise OverflowError("float overflow")
-    
     return int(a) + int(b)
