ffbb4dee6d79b60fd90132891c1b61afdc2a9831
clementgibot25 <10500@holbertonstudents.com>
2025-05-15 13:37:50 +0200

    verify a and b separately

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -16,6 +16,8 @@ def add_integer(a, b=98):
     Raises:
         TypeError: If either `a` or `b` is not an integer.
     """
-    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
-        raise TypeError("a must be an integer or b must be an integer")
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
     return int(a) + int(b)
