dcef6ca76494d97327832143f65e29b00ad7965e
clementgibot25 <10500@holbertonstudents.com>
2025-05-19 16:14:19 +0200

    fixed pycodestyle error

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -7,14 +7,14 @@ Module for Square class with private size attribute.
 class Square:
     """
     Defines a square with a private size attribute.
-    
+
     Attributes:
         __size (int): The size of the square (private).
     """
     def __init__(self, size):
         """
         Initialize a Square instance with a given size.
-        
+
         Args:
             size (int): The size of the square.
         """
