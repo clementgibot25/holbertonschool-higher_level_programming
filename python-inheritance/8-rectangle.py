262a8a12127c8062220f536008e74b12f26eb316
clementgibot25 <10500@holbertonstudents.com>
2025-05-30 11:13:20 +0200

    added comments for method

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -17,6 +17,10 @@ class Rectangle(BaseGeometry):
         Args:
             width (int): The width of the rectangle.
             height (int): The height of the rectangle.
+        
+        Raises:
+            TypeError: If width or height is not an integer.
+            ValueError: If width or height is less than or equal to 0.
         '''
         self.integer_validator("width", width)
         self.integer_validator("height", height)
