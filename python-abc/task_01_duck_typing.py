b2129b4a892662ee6eaf1d49f592008fd33cd48a
clementgibot25 <10500@holbertonstudents.com>
2025-05-30 14:57:14 +0200

    imported math module

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -23,9 +23,9 @@ class Circle(Shape):
     def __init__(self, radius):
         self.radius = radius
     def area(self):
-        return math.pi * self.radius * self.radius
+        return math.pi * (self.radius ** 2)
     def perimeter(self):
-        return 2 * math.pi * self.radius * self.radius
+        return 2 * math.pi * self.radius
 
 def shape_info(shape):
     print(f"Area: {shape.area()}")
