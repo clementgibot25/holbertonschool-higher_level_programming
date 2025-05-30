8873013d40bd4bf10874acb798f46c106e9b9608
clementgibot25 <10500@holbertonstudents.com>
2025-05-30 10:43:36 +0200

    added exceptions if value is not integer or if value is less or equal to 0

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+'''
+This module defines a class BaseGeometry with a method area.
+'''
+
+
+class BaseGeometry:
+    '''
+    A class with a method area that raises an exception.
+    '''
+
+    def area(self):
+        '''
+        Raises an exception indicating that the area method is not implemented.
+        '''
+        raise Exception("area() is not implemented")
+
+    def integer_validation(self, name, value):
+        '''
+        Validate that a value is an integer greater than 0.
+        '''
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
