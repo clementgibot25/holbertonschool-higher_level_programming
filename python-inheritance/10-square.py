f8ecbdcd49120b9b0f54d82efec5b279398a6605
clementgibot25 <10500@holbertonstudents.com>
2025-05-30 11:31:54 +0200

    created class Square that inherits from rectangle

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+'''
+This module defines a class Square that inherits from Rectangle.
+'''
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Square(BaseGeometry):
+    '''
+    A class that inherits from BaseGeometry.
+    '''
+
+    def __init__(self, size):
+        '''
+        Initialize a new Square instance.
+
+        Args:
+            size (int): The size of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than or equal to 0.
+        '''
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        '''
+        Calculate the area of the square.
+
+        Returns:
+            int: The area of the square (size * size).
+        '''
+        return self.__size * self.__size
