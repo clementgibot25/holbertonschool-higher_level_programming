959c223c0f1fdff633049e3296a688fe8e51ea1d
clementgibot25 <10500@holbertonstudents.com>
2025-05-30 11:58:13 +0200

    added super() from rectangle

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -3,12 +3,12 @@
 '''
 This module defines a class Square that inherits from Rectangle.
 '''
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
 
 
-class Square(BaseGeometry):
+class Square(Rectangle):
     '''
-    A class that inherits from BaseGeometry.
+    A class that inherits from Rectangle.
     '''
 
     def __init__(self, size):
@@ -19,7 +19,6 @@ class Square(BaseGeometry):
             size (int): The size of the square.
 
         Raises:
-            TypeError: If size is not an integer.
             ValueError: If size is less than or equal to 0.
         '''
         self.integer_validator("size", size)
