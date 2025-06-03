651e1c505bf2816145efd65732f3dac456603524
clementgibot25 <10500@holbertonstudents.com>
2025-06-03 14:21:08 +0200

    module that return object from JSON string

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+Module that returns an object (Python data structure) represented by a JSON string
+"""
+
+import json
+
+
+def from_json_string(my_str):
+    """returns an object (Python data structure) represented by a JSON string
+
+    args:
+        my_str: JSON string to be deserialized
+    """
+    return json.loads(my_str)
