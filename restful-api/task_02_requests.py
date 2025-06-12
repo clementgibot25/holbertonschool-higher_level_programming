80d13a1296cf31300ed1bcb0298086ef13abbf9c
clementgibot25 <10500@holbertonstudents.com>
2025-06-12 11:52:48 +0200

    fix print of status code success

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -14,7 +14,7 @@ def fetch_and_print_posts():
     """
     url = "https://jsonplaceholder.typicode.com/posts"
     response = requests.get(url)
-    print(response.status_code)
+    print(f"Status code: {response.status_code}")
     if response.status_code == 200:
         posts = response.json()
         for post in posts:
@@ -28,11 +28,11 @@ def fetch_and_save_posts():
     if response.status_code == 200:
         posts = response.json()
 
-posts_prepared = [{'id': post['id'], 'title': post['title'], 'body': post['body']} for post in posts]
+        posts_prepared = [{'id': post['id'], 'title': post['title'], 'body': post['body']} for post in posts]
 
-with open('posts.csv', 'w', newline='') as file:
-    writer = csv.DictWriter(file, fieldnames=['id', 'title', 'body'])
-    writer.writeheader()
-    writer.writerows(posts_prepared)
+        with open('posts.csv', 'w', newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=['id', 'title', 'body'])
+            writer.writeheader()
+            writer.writerows(posts_prepared)
 
-print("Posts saved to posts.csv")
+        print("Posts saved to posts.csv")
