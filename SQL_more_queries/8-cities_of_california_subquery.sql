57f576709e98073d576df00ce18270ac1ed252dc
clementgibot25 <10500@holbertonstudents.com>
2025-06-24 14:57:31 +0200

    remove db specifictation in code

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,4 @@
--- script that lists all the cities of California that can be found in the database hbtn_0d_usa
-USE hbtn_0d_usa;
-SELECT name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY cities.id ASC;
+-- Lists all cities of California found in the database
+SELECT id, name FROM cities 
+WHERE state_id = (SELECT id FROM states WHERE name = 'California')
+ORDER BY id ASC;
