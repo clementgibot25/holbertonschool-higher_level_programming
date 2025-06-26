49eb1abc76b92cfe95d460b78307f81fb62e6227
clementgibot25 <10500@holbertonstudents.com>
2025-06-26 14:28:11 +0200

    modify syntax

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,25 +1,39 @@
 #!/usr/bin/python3
 """
 Lists all states from the database hbtn_0e_0_usa
-Usage: ./0-select_states.py <mysql username> <mysql password> <database name>
 """
+import MySQLdb
+import sys
 
 if __name__ == "__main__":
-   from sys import argv
-   import MySQLdb
-
-   db = MySQLdb.connect(
+    # Get MySQL credentials from command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    
+    # Connect to MySQL server
+    db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3],
+        user=username,
+        passwd=password,
+        db=db_name,
         charset="utf8"
     )
-   cur = db.cursor()
-   cur.execute("SELECT * FROM states ORDER BY id ASC")
-   rows = cur.fetchall()
-   for row in rows:
-       print(row)
-   cur.close()
-   db.close()
+    
+    # Create a cursor object
+    cur = db.cursor()
+    
+    # Execute the SQL query
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    
+    # Fetch all rows
+    rows = cur.fetchall()
+    
+    # Print each row
+    for row in rows:
+        print(row)
+    
+    # Close cursor and database connection
+    cur.close()
+    db.close()
