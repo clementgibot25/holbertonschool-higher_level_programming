815ca06da24e753905a4653ef028d1d642c4d92c
clementgibot25 <10500@holbertonstudents.com>
2025-06-26 15:49:57 +0200

    lists states that starts with N

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Lists all states from the database hbtn_0e_0_usa that start with N."""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    # Connect to MySQL database
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+    )
+    
+    # Create cursor object using the cursor() method
+    cursor = db.cursor()
+    
+    # SQL query to fetch all states
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    
+    # Fetch all rows from the last executed statement
+    rows = cursor.fetchall()
+    
+    # Print each row
+    for row in rows:
+        print(row)
+    
+    # Close the database connection and cursor
+    cursor.close()
+    db.close()
+    
