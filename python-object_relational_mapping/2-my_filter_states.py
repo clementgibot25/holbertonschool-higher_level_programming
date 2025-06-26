da838f98b5e24be469260384b68a6b81078abfd3
clementgibot25 <10500@holbertonstudents.com>
2025-06-26 16:26:51 +0200

    takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,29 +1,31 @@
 #!/usr/bin/python3
-"""script that takes in an argument and displays
-all values in the states table of hbtn_0e_0_usa
-"""Lists all states where name matches the argument."""
+"""
+    script that takes in an argument and displays all values in the states
+    table of hbtn_0e_0_usa where name matches the argument
+"""
 
 import sys
 import MySQLdb
 
-if __name__ == "__main__":
-    user = sys.argv[1]
-    passwd = sys.argv[2]
-    db = sys.argv[3]
-    name = sys.argv[4]
 
-    conn = MySQLdb.connect(
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=user,
-        passwd=passwd,
-        db=db
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        database=sys.argv[3]
     )
-    cur = conn.cursor()
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(name)
-    cur.execute(query)
-    rows = cur.fetchall()
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name = BINARY '{}'\
+                    ORDER BY id ASC".format(sys.argv[4]))
+
+    rows = cursor.fetchall()
+
     for row in rows:
         print(row)
-    cur.close()
-    conn.close()
+
+    cursor.close()
+    db.close()
