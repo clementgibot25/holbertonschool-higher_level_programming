877c22497b05170f5bab01de6ff0e9c2675d2ea5
clementgibot25 <10500@holbertonstudents.com>
2025-06-27 13:13:19 +0200

    resyntaxing code

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,31 +1,33 @@
-#!/usr/bin/python3
-"""
-    script that takes in an argument and displays all values in the states
-    table of hbtn_0e_0_usa where name matches the argument (safe from SQL injection)
-"""
-
 import sys
-import MySQLdb
 
 if __name__ == "__main__":
+    # Connect to MySQL database
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
-        database=sys.argv[3]
+        db=sys.argv[3]
     )
 
-    cursor = db.cursor()
-    # Use parameterized query to avoid SQL injection
-    cursor.execute(
-        "SELECT * FROM states WHERE name = BINARY %s ORDER BY id ASC",
-        (sys.argv[4],)
-    )
+    # Get the state name to search for
+    state_name = sys.argv[4]
 
-    rows = cursor.fetchall()
+    # Create a cursor object
+    cur = db.cursor()
+
+    # Create SQL query using format()
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    # Execute the query
+    cur.execute(query, (state_name,))
+
+    # Fetch all the rows
+    rows = cur.fetchall()
+
+    # Print each row
     for row in rows:
         print(row)
 
-    cursor.close()
+    # Close cursor and database connection
+    cur.close()
     db.close()
