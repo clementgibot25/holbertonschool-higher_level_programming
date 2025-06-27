de9cb6a890aeb6ae5ee331a7e1a238f22f374fb6
clementgibot25 <10500@holbertonstudents.com>
2025-06-27 13:10:52 +0200

    list cities from db

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Script that lists all cities from the database hbtn_0e_4_usa
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Connection to the database
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    # Creation of cursor
+    cur = db.cursor()
+
+    # Execution of the query
+    cur.execute("""
+        SELECT cities.id, cities.name, states.name
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        ORDER BY cities.id ASC
+    """)
+
+    # Fetch and display results
+    for row in cur:
+        print(row)
+
+    # Close connections
+    cur.close()
+    db.close()
