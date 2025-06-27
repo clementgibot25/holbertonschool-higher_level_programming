a6c04ece82f470fee9865cef44453aac22234b4a
clementgibot25 <10500@holbertonstudents.com>
2025-06-27 14:10:20 +0200

    add new state object to db

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,43 +1,44 @@
 #!/usr/bin/python3
 """
-Script that adds the State object "Louisiana" to the database hbtn_0e_6_usa
+adds the State object “Louisiana” to the database hbtn_0e_6_usa
 """
 
+from sqlalchemy import (create_engine)
 import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import insert
 
 if __name__ == "__main__":
-    # Get command line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
 
-    # Create engine and connect to the database
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost:3306/{}'
-        .format(username, password, db_name),
-        pool_pre_ping=True
-    )
+    # Check input arguments
+    if len(sys.argv) != 4:
+        print(f"Usage: {sys.argv[0]} "
+              "<mysql username> <mysql password> <database name>")
+        sys.exit(1)
+
+    # DB connection
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    # Create tables
+    Base.metadata.create_all(engine)
 
     # Create a configured "Session" class
     Session = sessionmaker(bind=engine)
-
-    # Create a Session
     session = Session()
 
-    # Create a new State object
-    new_state = State(name="Louisiana")
-    
-    # Add the new state to the session
-    session.add(new_state)
-    
-    # Commit the transaction
+    add_state = insert(State).values(name='Louisiana')
+    session.execute(add_state)
     session.commit()
 
-    # Print the new state's id
-    print(new_state.id)
-
-    # Close the session
+    # Query and print the Louisiana state_id
+    states = session.query(State).order_by(State.id).filter(
+        State.name == 'Louisiana').all()
+    if states:
+        for state in states:
+            print(f"{state.id}")
+            break
+    else:
+        print("Not found")
     session.close()
