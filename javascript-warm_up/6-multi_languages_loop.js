56fc872edcddcf80f0d88ec34a9ffc264f57ce49
clementgibot25 <10500@holbertonstudents.com>
2025-07-16 14:34:44 +0200

    using array to print differents lines

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// Array of strings
+const messages = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+// Print each message using a for...of loop
+for (const message of messages) {
+  console.log(message);
+}
